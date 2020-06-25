60ec6cfd3159999f7dd032ee63366dd7a6d29c8a
Dorian Karter <dkarter@users.noreply.github.com>
2020-06-25 00:08:07 -0500

    Add priv/plts directory for CI

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule CookieMonster.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      dialyzer: dialyzer(),
 
       # Docs
       name: "CookieMonster",
@@ -27,6 +28,13 @@ defmodule CookieMonster.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp dialyzer do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
     ]
   end
 
